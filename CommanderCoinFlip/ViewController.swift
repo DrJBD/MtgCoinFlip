a25fcf9b4ecf7e4e44aee71319b98227ae866f36
Judah De Paula <depaula@gmail.com>
2019-03-08 22:23:15 -0600

    Initial Commit

---
+++ CommanderCoinFlip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommanderCoinFlip
+//
+//  Created by Judah De Paula on 3/8/19.
+//  Copyright © 2019 Judah De Paula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
